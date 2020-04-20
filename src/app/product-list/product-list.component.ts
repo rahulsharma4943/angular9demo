f0e088c00faa5e86be36b604d6f08cb7b36c2761
Rahul Sharma <rahul_shar@hcl.com>
2020-04-20 17:29:30 +0530

    first commit

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -13,6 +13,10 @@ export class ProductListComponent {
   share() {
     window.alert('The product has been shared!');
   }
+  onNotify(name:string)
+  {
+window.alert("You will be notified when "+name +" is available");
+  }
 }
 
 
